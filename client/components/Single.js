dd82c00eeb42b84d65160c4b36693b8263415530
avdkishore <avdkishore@MacBook-Air.local>
2016-09-25 18:12:43 +0530

    passing down all props to Comments component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,19 @@
 import React,{Component} from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 export default  class Single extends Component {
   render() {
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    const post = this.props.posts[i];
+
+    const postComments = this.props.comments[postId] || [];
+
     return(
-      <div>
-        Single
+      <div className="single-photo" >
+        <Photo post={post} index={i} {...this.props}/>
+        <Comments postComments={postComments} {...this.props}/>
       </div>
     );
   }
