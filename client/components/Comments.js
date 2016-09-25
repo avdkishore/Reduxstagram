b8f50ee57db095a104d94327b00fb95e5f94af20
avdkishore <avdkishore@MacBook-Air.local>
2016-09-25 18:11:46 +0530

    new Comments component added

---
+++ client/components/Comments.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+export default class Comments extends Component {
+
+  constructor() {
+    super(...arguments);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  renderComments(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    );
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props.params;
+    const author = this.refs.author.value ;
+    const comment = this.refs.comment.value ;
+
+    this.props.addComments(postId, author, comment);
+    this.refs.commentForm.reset();
+  }
+
+  render() {
+    return (
+      <div className="comments">
+        {this.props.postComments.map(this.renderComments)}
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit }>
+          <input type="text" ref="author" placeholder="author"/>
+          <input type="text" ref="comment" placeholder="comment"/>
+          <input type="submit" hidden/>
+        </form>
+      </div>
+    )
+  }
+};
